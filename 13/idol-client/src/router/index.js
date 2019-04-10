3cc70aa9744d825bb837c6bc4c8c0b90ddb813a9
having-Tech <david.jihoon92@gmail.com>
2019-04-10 21:17:21 +0900

    0410 작업일지

---
+++ 13/idol-client/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage' //추가
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+            path: '/',
+            name: 'Hello',
+            component: HelloWorld
+        },
+        {
+            path: '/example',
+            name: 'Example',
+            component: Example
+        },
+        { //추가
+            path: '/todos',
+            name: 'TodoPage',
+            component: TodoPage
+        }
+    ]
+});
